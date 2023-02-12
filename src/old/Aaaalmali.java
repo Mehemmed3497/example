ccddb53edc0e3116a11015e22f92451dc5140438
Ferid <feridxelilov708@gmail.com>
2023-02-12 14:48:30 +0400

    Men burda QAX elave eledim

--- src/old/Aaaalmali.java
+++ src/old/Aaaalmali.java
@@ -6,6 +6,7 @@ public class Aaaalmali {
     public static void main(String[] args) {
         for (int i = 0; i< 10; i++){
             System.out.println(i);
+            System.out.println("QAX");
         }
 
     }
